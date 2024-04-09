d6fb6d1c09dbbf47ae6227cea821ba56f27de63c
Gustavo Felix <gfnascimento@inf.ufsm.br>
2024-04-09 16:10:19 -0300

    Creanting command line arguments

---
+++ src/args.rs
@@ -0,0 +1,47 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+pub enum Options {
+    Encode {
+        #[structopt(parse(from_os_str))]
+        /// Path to file
+        file_path: PathBuf,
+
+        #[structopt()]
+        /// Chunk type
+        chunk_type: String,
+
+        #[structopt()]
+        /// Message to encode
+        message: String,
+
+        #[structopt(parse(from_os_str))]
+        /// Path to output file (optional)
+        output: PathBuf,
+    },
+    Decode {
+        #[structopt(parse(from_os_str))]
+        /// Path to file
+        file_path: PathBuf,
+
+        #[structopt()]
+        /// Chunk type
+        chunk_type: String,
+    },
+    Remove {
+        #[structopt(parse(from_os_str))]
+        /// Path to file
+        file_path: PathBuf,
+
+        #[structopt()]
+        /// Chunk type
+        chunk_type: String,
+    },
+    Print {
+        #[structopt(parse(from_os_str))]
+        /// Path to file
+        file_path: PathBuf,
+    },
+}
